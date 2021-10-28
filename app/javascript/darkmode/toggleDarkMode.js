f1f5ef487b41512cca4decad179332a0cddeb21a
Nathan Berthier <nathanberthier@outlook.fr>
2021-10-28 13:49:31 +0200

    Finish dark-mode

--- app/javascript/darkmode/toggleDarkMode.js
+++ app/javascript/darkmode/toggleDarkMode.js
@@ -2,8 +2,14 @@ const toggleDarkMode = () => {
   const btn = document.querySelector('.btn-dark');
   const body = document.querySelector('body');
 
+  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  if (userPrefersDark) {
+    body.classList.toggle("dark-theme")
+  }
+
   btn.addEventListener("click", () => {
-    body.classList.toggle("light-theme")
+    body.classList.toggle("dark-theme")
   });
 }
 
