86da7fe6576b407c91958cac446aee7d6176bda3
Nathan Berthier <nathanberthier@outlook.fr>
2021-10-30 23:22:19 +0200

    Change darkmode

--- app/javascript/darkmode/toggleDarkMode.js
+++ app/javascript/darkmode/toggleDarkMode.js
@@ -1,15 +1,25 @@
 const toggleDarkMode = () => {
   const btn = document.querySelector('.btn-dark');
   const body = document.querySelector('body');
-
+  const userValue = localStorage.getItem('darkMode');
+  const userPreference = userValue === 'true'
   const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-  if (userPrefersDark) {
-    body.classList.toggle("dark-theme")
+  if (userPreference && userValue) {
+    body.classList.add("dark-theme")
+  } else if (userPreference === false && userValue) {
+    body.classList.remove("dark-theme")
+  } else if (userPrefersDark) {
+    body.classList.add("dark-theme")
   }
 
   btn.addEventListener("click", () => {
     body.classList.toggle("dark-theme")
+    if (body.className === '') {
+      localStorage.setItem('darkMode', 'false');
+    } else {
+      localStorage.setItem('darkMode', 'true');
+    }
   });
 }
 
