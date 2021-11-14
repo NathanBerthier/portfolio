b1462e35036af8de05c40a259133f81e98796750
Nathan Berthier <nathanberthier@outlook.fr>
2021-11-14 15:37:14 +0100

    Adding edit and destroy btn for admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
   def index
     @posts = Post.all
   end
